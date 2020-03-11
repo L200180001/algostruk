534a8dad728ed80a369f9dc909e243f786b81c37
L200180001 <48128820+L200180001@users.noreply.github.com>
2020-03-12 00:02:41 +0700

    Add files via upload

---
+++ MODUL_2/no6.py
@@ -0,0 +1,32 @@
+class Manusia(object):
+    """Class 'Manusia' dengan inisiasi 'nama'"""
+    keadaan = 'lapar'
+    
+    def __ini__(self, nama):
+        self.nama = nama
+    def ucapkanSalam(self):
+        print('Salam, namaku',self.nama)
+    def makan(self, s):
+        print('Saya baru saja makan', s)
+        self.keadaan = 'kenyang'
+    def olahraga(self, k):
+        print('Saya baru saja latihan', k)
+        self.keadaan = 'lapar'
+    def mengalikanDenganDua(self, n):
+        return n*2
+
+class SiswaSMA(Manusia):
+    def __init__(self, nama, NIS, umur, us):
+        """Metode inisiasi ini menutupi metode inisiasi di class Manusia"""
+        self.nama = nama
+        self.nis = NIS
+        self.umur = umur
+        self.us = us
+        
+    
+    def __str__(self):
+        return "\n\nData Diri\n"\
+               +"Nama   : "+self.nama\
+               +"\nNIS    : "+self.nis\
+               +"\nUmur : " +self.umur
+   
